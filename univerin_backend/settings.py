e952fb99936b4c71685995527e7c3360a52a29d6
Sravani Polamreddy <sravlaks18112015@gmail.com>
2026-05-11 21:23:14 -0400

    Fix Cloudinary storage for Django 4.2+

--- univerin_backend/settings.py
+++ univerin_backend/settings.py
@@ -153,3 +153,11 @@ cloudinary.config(
 )
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
